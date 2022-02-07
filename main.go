aa05f876402e66910ec7f133188c82c6c25c2f21
fujitoshion <43021337+FUJITOSHION@users.noreply.github.com>
2022-02-07 21:59:08 +0900

    update

--- main.go
+++ main.go
@@ -9,8 +9,7 @@ import (
 	"github.com/line/line-bot-sdk-go/linebot"
 )
 
-func helloHandler(w http.ResponseWriter, r *http.Request) {
-	msg := linebot.NewTextMessage("hello, world")
+func loadEnv() (bot *linebot.Client) {
 	bot, err := linebot.New(
 		os.Getenv("LINE_BOT_CHANNEL_SECRET"),
 		os.Getenv("LINE_BOT_CHANNEL_TOKEN"),
@@ -18,6 +17,12 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		log.Fatal(err)
 	}
+	return bot
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	msg := linebot.NewTextMessage("hello, world")
+	bot := loadEnv()
 	// テキストメッセージを友達登録しているユーザー全員に配信する
 	if _, err := bot.BroadcastMessage(msg).Do(); err != nil {
 		log.Fatal(err)
@@ -25,29 +30,22 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func lineHandler(w http.ResponseWriter, r *http.Request) {
-	bot, err := linebot.New(
-		os.Getenv("LINE_BOT_CHANNEL_SECRET"),
-		os.Getenv("LINE_BOT_CHANNEL_TOKEN"),
-	)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	message := linebot.NewTextMessage("hello, world2")
-	// テキストメッセージを友達登録しているユーザー全員に配信する
-	if _, err := bot.BroadcastMessage(message).Do(); err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("r", r)
-	fmt.Println("body", r.Body)
+	bot := loadEnv()
 	events, _ := bot.ParseRequest(r)
 	for _, event := range events {
 		fmt.Println(event.Message)
+		if event.Type == linebot.EventTypeMessage {
+			switch message := event.Message.(type) {
+			case *linebot.TextMessage:
+				if _, err := bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.Text)).Do(); err != nil {
+					log.Print(err)
+				}
+			}
+		}
 	}
 }
 
 func main() {
-	// http.HandleFunc("/", lineHandler)
 	server := http.Server{
 		Addr:    ":" + os.Getenv("PORT"),
 		Handler: nil,
