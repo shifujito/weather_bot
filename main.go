280ae453067e211524f73cf3b9450c04a1048db6
fujitoshion <43021337+FUJITOSHION@users.noreply.github.com>
2022-02-07 21:15:29 +0900

    update

--- main.go
+++ main.go
@@ -6,34 +6,25 @@ import (
 	"net/http"
 	"os"
 
-	"github.com/joho/godotenv"
 	"github.com/line/line-bot-sdk-go/linebot"
 )
 
-func loadEnv() {
-	err := godotenv.Load(".env")
-	if err != nil {
-		fmt.Print("環境変数が読み込めませんでした。")
-		fmt.Print(err)
-	}
-}
 func helloHandler(w http.ResponseWriter, r *http.Request) {
-	msg := "Hello World!!!!"
-	fmt.Fprintf(w, msg)
-	loadEnv()
+	msg := linebot.NewTextMessage("hello, world")
 	bot, err := linebot.New(
 		os.Getenv("LINE_BOT_CHANNEL_SECRET"),
 		os.Getenv("LINE_BOT_CHANNEL_TOKEN"),
 	)
 	if err != nil {
-		fmt.Println("環境変数", err)
+		log.Fatal(err)
 	}
-	events, _ := bot.ParseRequest(r)
-	fmt.Println("events", events)
+	// テキストメッセージを友達登録しているユーザー全員に配信する
+	if _, err := bot.BroadcastMessage(msg).Do(); err != nil {
+		log.Fatal(err)
+	}
 }
 
 func lineHandler(w http.ResponseWriter, r *http.Request) {
-	loadEnv()
 	bot, err := linebot.New(
 		os.Getenv("LINE_BOT_CHANNEL_SECRET"),
 		os.Getenv("LINE_BOT_CHANNEL_TOKEN"),
